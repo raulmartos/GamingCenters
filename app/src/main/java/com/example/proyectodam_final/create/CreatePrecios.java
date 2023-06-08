63d5f79688d2b11f9f88f0982d207578113e7132
raulmartos <raulmarriv@gmail.com>
2023-06-08 20:06:15 +0200

    Funcionalidad de precios, actualiza los datos de la bbdd

--- app/src/main/java/com/example/proyectodam_final/create/CreatePrecios.java
+++ app/src/main/java/com/example/proyectodam_final/create/CreatePrecios.java
@@ -1,19 +1,81 @@
 package com.example.proyectodam_final.create;
 
+import android.view.View;
+import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import com.example.proyectodam_final.R;
+import com.example.proyectodam_final.pojo.Price;
+import com.google.firebase.database.*;
 
 
 public class CreatePrecios extends AppCompatActivity {
 
+    private Spinner spTipo;
+    private EditText edtPrice;
+    private Button btAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_precios);
-
-
+        elementMatcher();
+        spinnerInfo();
+        btAdd.setOnClickListener(v -> updatePrice());
     }
+
+    private void elementMatcher() {
+        edtPrice = findViewById(R.id.edtPrecioEntrada);
+        spTipo = findViewById(R.id.spTipo);
+        btAdd = findViewById(R.id.btnSavePrecio);
+    }
+
+    private void spinnerInfo() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tipo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTipo.setAdapter(adapter);
+    }
+
+    private void updatePrice() {
+        int selectedPosition = spTipo.getSelectedItemPosition();
+        if (selectedPosition == 0) {
+            Toast.makeText(CreatePrecios.this, "Debes seleccionar un tipo", Toast.LENGTH_SHORT).show();
+        } else {
+            String type = spTipo.getSelectedItem().toString();
+            String priceText = edtPrice.getText().toString();
+            if (priceText.isEmpty()) {
+                Toast.makeText(CreatePrecios.this, "Por favor, ingresa un precio", Toast.LENGTH_SHORT).show();
+            } else {
+                double price = Double.parseDouble(priceText);
+
+                DatabaseReference pricesRef = FirebaseDatabase.getInstance().getReference().child("prices");
+                pricesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        int currentPosition = 1;
+                        for (DataSnapshot priceSnapshot : dataSnapshot.getChildren()) {
+                            if (currentPosition == selectedPosition) {
+                                Price updatedPrice = new Price(type, price);
+                                priceSnapshot.getRef().setValue(updatedPrice);
+                                Toast.makeText(CreatePrecios.this, "Precio actualizado exitosamente", Toast.LENGTH_SHORT).show();
+                                finish();
+                                return;
+                            }
+                            currentPosition++;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(CreatePrecios.this, "Error al actualizar el precio", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/proyectodam_final/pojo/Price.java
@@ -0,0 +1,27 @@
+package com.example.proyectodam_final.pojo;
+
+public class Price {
+    private String type;
+    private Double price;
+
+    public Price(String type, Double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
